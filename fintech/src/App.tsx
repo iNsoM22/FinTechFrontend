c5fc2202dcb30114e3d73b1d57b2ea1a4c42f92a
Hafsa-Ahmed77 <javeriaahmed561@gmail.com>
2025-04-22 14:52:51 +0500

    Pricing-page

--- fintech/src/App.tsx
+++ fintech/src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router"
 
 function App() {
   return<>
   <Outlet />;
-  <Toaster position="top-right" />
+  <Toaster />
   
   </>
 }
